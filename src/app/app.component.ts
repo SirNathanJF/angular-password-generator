fc8176f0ba32de976e9c56c2b9bf27aedebfb242
Nathan Flessner <nathan.flessner@arrow.com>
2022-05-13 11:48:32 -0600

    initialization of onButtonClick function, and placing the generated password into the DOM

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-password-generator';
+  password: string = '';
+
+  onButtonClick() {
+    this.password = 'MY PASSWORD!!!!';
+    console.log(this.password);
+  }
 }
